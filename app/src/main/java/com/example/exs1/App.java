51476f59c8c3ed6b76149a37e5b6e7409f27a909
Yoav Herman <62719732+yoav1995@users.noreply.github.com>
2023-05-14 17:37:59 +0300

    my last commit

--- app/src/main/java/com/example/exs1/App.java
+++ app/src/main/java/com/example/exs1/App.java
@@ -1,9 +1,15 @@
 package com.example.exs1;
 
 import android.app.Application;
+import android.util.Log;
 
+import com.example.exs1.Models.HighScore;
+import com.example.exs1.Utilities.DataManager;
 import com.example.exs1.Utilities.MySP;
 import com.example.exs1.Utilities.SignalGen;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
 
 
 public class App extends Application {
@@ -11,17 +17,19 @@ public class App extends Application {
 
 
         @Override
+
         public void onCreate() {
             super.onCreate();
-            String json = "{\"highScores\":[{\"rankTitle\":Yoav Herman,\"score\":5000},{\"rankTitle\":Yoav ,\"score\":4000},{\"rankTitle\":Herman,\"score\":3000}]}";
+            ArrayList<HighScore>  highScore = DataManager.getHighScores();
+            DataManager highscores= new DataManager();
+            String highscoreList=new Gson().toJson(highscores);
+            Log.d("JSON:", highscoreList);
             SignalGen.init(this);
             MySP.init(this);
-            MySP.getInstance().putString("EXS2", json);
+            MySP.getInstance().putString("EXS2", highscoreList);
             String fromSP = MySP.getInstance().getString("EXS2","");
-
-            //don't work properly
-            //DataManager highscoreListFromJson = new Gson().fromJson(fromSP,DataManager.class);
-
+            DataManager highscoreListFromJson = new Gson().fromJson(fromSP,DataManager.class);
+            Log.d(" HighScore from JSON:", highscoreListFromJson.toString());
         }
     }
 
--- app/src/main/java/com/example/exs1/Utilities/DataManager.java
+++ app/src/main/java/com/example/exs1/Utilities/DataManager.java
@@ -8,13 +8,11 @@ public class DataManager {
     private ArrayList<HighScore> highScores = new ArrayList<>();
 
     public DataManager() {
+
     }
 
     public static ArrayList<HighScore> getHighScores(){
         ArrayList<HighScore> highscores=new ArrayList<>();
-
-
-        /*
         /// hard coded!
         highscores.add(new HighScore()
                 .setRankTitle("Yoav Herman").setScore(5000)
@@ -25,7 +23,7 @@ public class DataManager {
         highscores.add(new HighScore()
                 .setRankTitle(" Herman").setScore(3000)
         );
-        */
+
         return highscores;
     }
 }
